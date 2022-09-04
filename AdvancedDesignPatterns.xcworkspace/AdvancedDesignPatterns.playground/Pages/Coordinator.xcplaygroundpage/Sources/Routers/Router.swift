cf1541cafa89947eb55eb530ebfe904ef45152ab
Robert <robert.ios.developer@gmail.com>
2022-09-03 23:02:10 -0600

     :building_construction: added router protocol

--- AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/Router.swift
+++ AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/Router.swift
@@ -1,1 +1,20 @@
+import UIKit
 
+public protocol Router: class {
+  func present(_ viewController: UIViewController,
+                animated: Bool)
+                
+  func present(_ viewController: UIViewController,
+               animated: Bool,
+               onDismissed: (()->Void)?)
+  func dismiss(animated: Bool)
+}
+
+extension Router {
+  public func present(_ viewController: UIViewController,
+                      animated: Bool) {
+    present(viewController,
+            animated: animated,
+            onDismissed: nil)
+  }
+}
