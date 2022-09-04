9e91a4bd9831ab81234edf1f0ca2b2387516b503
Robert <robert.ios.developer@gmail.com>
2022-09-04 17:56:24 -0600

    :poop: added router protocol implementations

--- AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/NavigationRouter.swift
+++ AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/NavigationRouter.swift
@@ -13,5 +13,47 @@ public class NavigationRouter: NSObject {
     self.routerRootController =
       navigationController.viewControllers.first
     super.init()
+      self.navigationController.delegate = self
   }
 }
+
+extension NavigationRouter: Router {
+    public func present(_ viewController: UIViewController, animated: Bool, onDismissed: (() -> Void)?) {
+        onDismissForViewController[viewController] = onDismissed
+        navigationController.pushViewController(viewController,
+                                                    animated: animated)
+    }
+    
+    public func dismiss(animated: Bool) {
+        guard let routerRootController = routerRootController else {
+            navigationController.popToRootViewController(animated: animated)
+            return }
+    }
+    
+    private func performOnDismissed(for viewController: UIViewController) {
+        guard let onDissmiss = onDismissForViewController[viewController] else { return }
+        onDissmiss()
+        onDismissForViewController[viewController] = nil
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension NavigationRouter: UINavigationControllerDelegate {
+    
+    public func navigationController(
+        _ navigationController: UINavigationController,
+        didShow viewController: UIViewController,
+        animated: Bool) {
+            
+            guard let dismissedViewController =
+                    navigationController.transitionCoordinator?
+                .viewController(forKey: .from),
+                  !navigationController.viewControllers
+                .contains(dismissedViewController) else {
+                return
+            }
+            performOnDismissed(for: dismissedViewController)
+        }
+}
+
+
--- AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/Router.swift
+++ AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/Router.swift
@@ -18,3 +18,4 @@ extension Router {
             onDismissed: nil)
   }
 }
+
