69bae3c4c0d0e5b85f25e80239301ea676eb43d1
Robert <robert.ios.developer@gmail.com>
2022-09-03 23:02:53 -0600

    :building_construction: added navigation router

--- AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/NavigationRouter.swift
+++ AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Routers/NavigationRouter.swift
@@ -1,1 +1,17 @@
 
+import UIKit
+
+public class NavigationRouter: NSObject {
+
+  private let navigationController: UINavigationController
+  private let routerRootController: UIViewController?
+  private var onDismissForViewController:
+    [UIViewController: (() -> Void)] = [:]
+
+  public init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
+    self.routerRootController =
+      navigationController.viewControllers.first
+    super.init()
+  }
+}
