7e4c774e62a2fe871d84d155838132e6ba618c1e
Robert <robert.ios.developer@gmail.com>
2022-09-04 18:22:43 -0600

    :label: added coordinator protocols

--- AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Coordinators/Coordinator.swift
+++ AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Coordinator.xcplaygroundpage/Sources/Coordinators/Coordinator.swift
@@ -1,1 +1,41 @@
+public protocol Coordinator: AnyObject {
+    
+    var children: [Coordinator] { get set }
+    var router: Router { get }
+    
+    func present(animated: Bool, onDismissed: (() -> Void)?)
+    func dismiss(animated: Bool)
+    func presentChild(_ child: Coordinator, animated: Bool, onDismissed: (() -> Void)?)
+    
+}
+
+extension Coordinator {
+    
+    public func dismiss(animated : Bool ) {
+        router.dismiss(animated: animated)
+    }
+    
+    public func presentChild(_ child: Coordinator, animated: Bool, onDismissed: (() -> Void)? = nil) {
+        children.append(child)
+        child.present(animated: animated) { [weak self, weak child] in
+            guard let self = self,
+                  let child = child else {
+                return
+            }
+            self.removeChild(child)
+            onDismissed?()
+        }
+        
+    }
+    
+    private func removeChild(_ child: Coordinator) {
+        guard let index = children.firstIndex(
+            where: { $0 === child }) else {
+            return
+        }
+        children.remove(at: index)
+    }
+    
+}
+
 
